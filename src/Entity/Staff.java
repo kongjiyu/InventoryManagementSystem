76fd72613311cd3557a24e7f32bef91cfed72ee4
Kong Ji Yu <kongjiyu0198@gmail.com>
2024-07-21 15:38:59 +0800

    Added Renjun Function

--- src/Entity/Staff.java
+++ src/Entity/Staff.java
@@ -46,4 +46,8 @@ public class Staff {
     public void printHello(){
         System.out.println("Hello World!");
     }
+
+    public void renJunFunction(){
+        System.out.println("I'm Ren Jun");
+    }
 }
