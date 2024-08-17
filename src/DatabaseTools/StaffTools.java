0c3d0301bd5289650c053dd3d9b72bbc414aea24
Kong Ji Yu <kongjiyu0198@gmail.com>
2024-08-17 09:38:40 +0800

    Update check staff ID

--- src/DatabaseTools/StaffTools.java
+++ src/DatabaseTools/StaffTools.java
@@ -1,4 +1,27 @@
 package DatabaseTools;
 
+import Database.DatabaseUtils;
+import Entity.Staff;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class StaffTools {
+    public boolean checkStaffID(String staffID){
+        Staff staff = null;
+
+        Connection connection = DatabaseUtils.getConnection();
+        String sql = "SELECT * FROM staff WHERE StaffID = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, staffID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet.next();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
