4a6319b7e28af5fe401cbcf5975cdf0c5c5ef881
Kong Ji Yu <kongjiyu0198@gmail.com>
2024-08-17 16:09:57 +0800

    Add insert staff function

--- src/DatabaseTools/StaffTools.java
+++ src/DatabaseTools/StaffTools.java
@@ -2,11 +2,9 @@ package DatabaseTools;
 
 import Database.DatabaseUtils;
 import Entity.Staff;
+import Entity.Admin;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class StaffTools {
     public boolean checkStaffID(String staffID){
@@ -25,5 +23,47 @@ public class StaffTools {
         }
     }
 
-    public boolean checkStaffAccount(String staffID, String password)
+    public boolean checkStaffAccount(String staffID, String password){
+        return true;
+    }
+
+
+
+    public static boolean registerStaff(Staff staff){
+        //get connection
+        Connection connection = DatabaseUtils.getConnection();
+        //set the sql statement
+        String sql = "INSERT INTO staff VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+        try {
+            //create a prepared statement
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            //set the parameters
+            preparedStatement.setString(1, staff.getStaffID());
+            preparedStatement.setString(2, staff.getPassword());
+            preparedStatement.setString(3, staff.getStaffIC());
+            preparedStatement.setString(4, staff.getName());
+            preparedStatement.setInt(5, staff.getAge());
+            preparedStatement.setDate(6, Date.valueOf(staff.getHireDate()));
+            preparedStatement.setDate(7, Date.valueOf(staff.getBirthDate()));
+            preparedStatement.setDouble(8, staff.getSalary());
+            preparedStatement.setString(9, staff.getEmail());
+            preparedStatement.setString(10, staff.getPhone());
+            preparedStatement.setString(11, staff.getAddress());
+            //check is the staff admin
+            if(staff instanceof Admin){
+                preparedStatement.setInt(12, 1);
+            }else{
+                preparedStatement.setNull(12, Types.INTEGER);
+            }
+
+            int result = preparedStatement.executeUpdate();
+
+            return result > 0;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
